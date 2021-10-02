e3e025ee0657a6b590cc9a73874a45beffb0c62d
Anand <anand@alphasheets.com>
2021-10-02 12:39:54 -0400

    recurrence plots

--- gds/examples/turbulence.py
+++ gds/examples/turbulence.py
@@ -16,12 +16,12 @@ from .fluid_projected import *
 
 def poincare_section():
 	# Define system
-	G = gds.triangular_lattice(m=1, n=5)
+	G = gds.triangular_lattice(m=1, n=2)
 	N_e = len(G.edges())
 	y0 = np.random.uniform(low=1, high=2, size=N_e)
 
 	# Energies
-	KE = np.linspace(1, 15, 5)
+	KE = np.linspace(1, 10, 10)
 	fig, axs = plt.subplots(nrows=1, ncols=len(KE), figsize=(len(KE)*5, 5))
 
 	# Define transverse hyperplane
@@ -40,16 +40,50 @@ def poincare_section():
 		y0_ *= np.sqrt(ke / np.dot(y0_, y0_))
 		V.set_initial(y0=lambda e: y0_[V.X[e]])
 		sys = gds.couple({'V': V, 'P': P})
-		time, data = sys.solve(10, 0.01)
+		time, data = sys.solve(20, 0.01)
 		b = V.y[i]
 		section = data['V'][np.round(data['V'] @ a, 2) == np.round(b, 2)]  
-		axs[fig_idx].scatter(section[:, j], section[:, k], s=5**2)
+		axs[fig_idx].scatter(section[:, j], section[:, k], s=5)
 		axs[fig_idx].set_title(f'KE: {ke}')
+
+	plt.tight_layout()
+	plt.show()
+
+def recurrence_plot():
+	eps = 1e-2
+	n_triangles = list(range(2, 6))
+	energies = np.logspace(-1, 2, 5)
+
+	fig, axs = plt.subplots(nrows=len(n_triangles), ncols=len(energies), figsize=(len(energies)*5, len(n_triangles)*5))
+
+	for fig_i, N in enumerate(n_triangles):
+		G = gds.triangular_lattice(m=1, n=N)
+		N_e = len(G.edges())
+		y0 = np.random.uniform(low=1, high=2, size=N_e)
+		for fig_j, KE in enumerate(energies):
+			V, P = euler(G)
+			y0_ = V.leray_project(y0)
+			y0_ *= np.sqrt(KE / np.dot(y0_, y0_))
+			V.set_initial(y0=lambda e: y0_[V.X[e]])
+			sys = gds.couple({'V': V, 'P': P})
+			time, data = sys.solve(10, 0.01)
+			points = []
+			for r in range(time.size):
+				for c in range(time.size):
+					if np.linalg.norm(data['V'][r] - data['V'][c]) <= eps:
+						points.append([time[r], time[c]])
+			points = np.array(points)
+			axs[fig_i][fig_j].scatter(points[:,0], points[:,1], s=1)
+			if fig_i == 0:
+				axs[fig_i][fig_j].set_title(f'KE: {KE}')
+			if fig_j == 0:
+				axs[fig_i][fig_j].set_ylabel(f'N: {N}')
 
 	plt.tight_layout()
 	plt.show()
 
 
 if __name__ == '__main__':
-	gds.set_seed(10)
-	poincare_section()
+	gds.set_seed(1)
+	# poincare_section()
+	recurrence_plot()
